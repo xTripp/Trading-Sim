17b476508b35f01849b692753f3cb9a9fca447c3
Nick - Work <Nick.Koelble@gmail.com>
2024-02-21 18:46:57 -0500

    second commit

--- main.js
+++ main.js
@@ -6,18 +6,18 @@ api_key.apiKey = process.env.API_KEY;
 const finnhubClient = new finnhub.DefaultApi();
 
 function getPrice(symbol) {
-    try {
-        finnhubClient.quote(symbol, (err, data, response) => {
-            if (err) {
-                console.log(err.status);
-            }
-            console.log(data);
-        })
-    } catch(err) {
-        console.error(`Error: ${err}`);
-    }
+    finnhubClient.quote(symbol, (err, data, response) => {
+        if (err) {
+            console.error(`Error: ${err}`);
+            console.error(err.status);
+            return;
+        }
+        console.log(response.headers['x-ratelimit-remaining']);
+        console.log(response.headers['x-ratelimit-reset']);
+        console.log(data);
+    })
 }
 
 setInterval(() => {
     getPrice('COIN');
-}, 100);
+}, 800);
