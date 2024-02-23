9c3fd2eb1297c36e0653fe98a01c78684d7491cb
Nick - Work <Nick.Koelble@gmail.com>
2024-02-23 10:31:55 -0500

    save progress

--- exchange.js
+++ exchange.js
@@ -5,16 +5,19 @@ const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = process.env.API_KEY;
 const finnhubClient = new finnhub.DefaultApi();
 
-function getPrice(symbol, vals=['c']) {
+function getPrice(symbol, vals=['c'], callback) {
     finnhubClient.quote(symbol, (err, data, response) => {
         if (err) {
-            console.error(`Error ${err.status}: ${err}`);
+            console.error(`Error status ${err.status}: ${err}`);
+            callback(err, vals);
             return;
         }
 
-        const info = Array.from(Object.keys(data)).filter(val => val in vals);
-        return info;
-    })
+        const info = Object.fromEntries(
+            Object.keys(data).filter(key => vals.includes(key)).map(key => [key, data[key]])
+        );
+        callback(null, info);
+    });
 }
 
 function apiStats() {
--- traders/basic/basic.js
+++ traders/basic/basic.js
@@ -4,7 +4,16 @@ const ex = require('../../exchange.js');
 const basic = new Portfolio('basic');
 
 function buyLowSellHigh() {
-    let previousPrice = ex.getPrice('COIN');
+    let previousPrice = ex.getPrice('COIN', ['c'], (err, data) => {
+        if (err) {
+            return data.reduce((val, i) => {
+                val[i] = 0.00;
+                return val;
+            }, {});
+        } else {
+            return data;
+        }
+    });
     console.log(previousPrice);
 }
 
